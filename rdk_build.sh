4dff93d2b394dff2c895db816aeffca2c2d178e1
Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>
2022-06-27 15:55:36 -0400

    RDKC-10711 : Add rpath as RDK-C glibc update is not picking dependent libs
    Reason for change: Add rpath as RDK-C glibc update is not picking dependent libs
    Test Procedure: Successful build
    Risks: Low
    Signed-off-by: Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>
    
    (cherry picked from commit 92c3f66c1191ec7f6c073359262ffaf5c9652bd5)
    Change-Id: I939774c2c286340d5b4acbde929e4d946a4931d8

--- rdk_build.sh
+++ rdk_build.sh
@@ -64,24 +64,12 @@ elif [ "$XCAM_MODEL" == "SERXW3" ] || [ "$XCAM_MODEL" == "SERICAM2" ] || [ "$XCA
 elif [ "$XCAM_MODEL" == "XHC3" ]; then
     echo "Setting environmental variables and Pre rule makefile for XHC3"
     source ${RDK_PROJECT_ROOT_PATH}/build/components/sdk/setenv2
-    RDK_PATCHES=$RDK_PROJECT_ROOT_PATH/build/components/opensource/patch
-    if [ ! -f $RDK_PATCHES/.rbuscore.patched ]; then
-        echo "applying patch for XHC3 rbuscore"
-        cd ${RDK_PROJECT_ROOT_PATH}/rbuscore
-        git apply $RDK_PATCHES/rbuscore.diff
-        touch $RDK_PATCHES/.rbuscore.patched
-        cd -
-    else
-        echo "Patch already applied so going ahead with the build"
-    fi
 else #No Matching platform
         echo "Source environment that include packages for your platform. The environment variables PROJ_PRERULE_MAK_FILE should refer to the platform s PreRule make"
 fi
 
-#export COMP_BASE_PATH=${RDK_SCRIPTS_PATH%/*}
-export COMP_BASE_PATH=${RDK_SCRIPTS_PATH}/../
-export SEARCH_PATH=$COMP_BASE_PATH/opensource
-export INSTALL_PATH=$COMP_BASE_PATH/sdk/fsroot/ramdisk/usr
+export SEARCH_PATH="$RDK_FSROOT_PATH/usr;$RDK_FSROOT_PATH/usr/local"
+export INSTALL_PATH=$RDK_FSROOT_PATH/usr
 
 #The cross compile tools are exported already in XHB1;lets avoid only that (Because SOURCETOOLCHAIN is not exported in XHB1)
 if [ "$XCAM_MODEL" != "XHB1" ]; then
@@ -130,9 +118,6 @@ done
 
 ARGS=$@
 
-
-# component-specific vars
-export FSROOT=${RDK_FSROOT_PATH}
 # functional modules
 
 function configure()
@@ -142,7 +127,7 @@ function configure()
     mkdir -p ${RDK_PROJECT_ROOT_PATH}/rbuscore/build
     cd ${RDK_PROJECT_ROOT_PATH}/rbuscore/build
 
-    cmake  -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_PREFIX_PATH=${SEARCH_PATH} -DENABLE_RDKLOGGER=ON -DRBUS_ALWAYS_ON=ON ..
+    cmake  -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_PREFIX_PATH=${SEARCH_PATH} -DENABLE_RDKLOGGER=ON -DRBUS_ALWAYS_ON=ON -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath-link,${RDK_FSROOT_PATH}/usr/lib" ..
 }
 
 function clean()
